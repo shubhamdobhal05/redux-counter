f63957b44f66eccf2c006a163123359da1014abe
shubhamdobhal05 <shubhamdobhal05@gmail.com>
2022-06-01 22:00:53 +0530

    updated

---
+++ src/component/Counter.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Counter = () => {
+    return(
+        <div>
+        <h1>Count : </h1>
+            <button>ADD</button>
+            <button>DELETE</button>
+        </div>
+    )
+}
+
+export default Counter;
